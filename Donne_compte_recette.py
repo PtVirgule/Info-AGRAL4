75d362780020b5634e58c1b66577b980830ad5a4
Vir <virginie2002.maurin@orange.fr>
2023-12-27 21:22:29 +0100

    Mise a jour

--- Donne_compte_recette.py
+++ Donne_compte_recette.py
@@ -1,43 +1,84 @@
 import random
 from Healthy import est_healthy
+from Creation_dic_recette_indexe2 import donne_dico_index
+from Part1 import donne_liste_recette
 
-dic = {1: ['Salade', 'Pizza'], 2: ['Mayonnaise', 'Gnockie', 'Cookie'], 3: ['Repas']}
-dic2 = {1: ['Pizza Végétarienne']}
+#dic = {1: ['Salade', 'Pizza'], 2: ['Mayonnaise', 'Gnockie', 'Cookie'], 3: ['Repas']}
+#dic2 = {1: ['Pizza Végétarienne'], 2: ['Salade', 'Pizza'], 3: ['Bruschetta', 'Quiche aux Légumes'], 4: ['Potage de Légumes']}
 
-# il faur prendre en compte que els recettes sont classées par quantité d'ingrédients souhaités : faut mettre dans
-# liste tot_ingre_healthy que les recetees avec l'index max (et si le compte est trop faible prendre celui d'aant
-# aussi))
 
-#mettre à jour : donne_compte_recette + healthy
+liste_ingre = [' Champignons', 'Tofu', 'Poulet', ' Riz basmati cuit','Quinoa']
+li = donne_liste_recette()
+
+
+def donne_liste_cle(cle_dico):
+    li_cle = []
+    for cle in cle_dico:
+        li_cle.append(cle)
+    return li_cle
+
+
+def donne_cle_max(liste_cle):
+    if not (liste_cle):  # cas où la liste est vide
+        return -1
+    else:
+        # trouver l'élement max d'une liste
+        max_liste = liste_cle[0]
+        for i in liste_cle:
+            if max_liste < i:
+                max_liste = i
+        return max_liste
+
+
+def est_complet_liste_retour(lis):
+    if len(lis) == 0:
+        return False
+    else:
+        if len(lis[0]) == 0 or len(lis[1]) == 0: #cas où un des élements de la liste est vide donc qu'il n'y a pas de proposition d'un type de recette (healthy / non helthy)
+            return False
+        else:
+            return True
+
+# mettre à jour : donne_compte_recette + healthy
 def donne_bon_compte_recette(dic):
-    print(dic.keys())
-    # Cas où le dictionnaire es vide
-    if len(dic) == 0:
-        return [0, 0]
-    else:  # cas où le dictionnaire contient au moins 1 recette
-        # On compte le nombre de recette totale dans le dictionnaire
-        nombre_tot_recette_possible = 0
-        for i in dic.keys():
-            nombre_tot_recette_possible += len(dic[i])
-        print(nombre_tot_recette_possible)
-        liste_recette_ingre_healthy = []
-        liste_recette_ingre_non_healthy = []
-        for i in dic.keys():  # sachant que dans ce cas il y a une seule clé
-            if est_healthy(dic[i]):
-                liste_recette_ingre_healthy.append(dic[i])
+    compte_recette = []
+    liste_cle = donne_liste_cle(dic.keys())
+
+    liste_recette_ingre_healthy = []
+    liste_recette_ingre_non_healthy = []
+    while est_complet_liste_retour(
+            compte_recette) == False and donne_cle_max(liste_cle) != -1:  # tant qu'on n'a pas au moins 1 recette healthy et non healty
+        # but récuperer liste des clé (pour ensuite trouver la clé max)
+        # si il n'y a plus de clé cad le dictionnaire est à nouveau vide :
+        if len(dic) == 0:
+            return [0, 0]
+        else:
+            indice_max = donne_cle_max(liste_cle)
+
+        # remplir la liste des recettes avec les recettes ayant l'indice indice_max
+        for i in range(len(dic[indice_max])):
+            if est_healthy(dic[indice_max][i]):
+                liste_recette_ingre_healthy.append(dic[indice_max][i])
             else:
-                liste_recette_ingre_non_healthy.append(dic[i])
+                liste_recette_ingre_non_healthy.append(dic[indice_max][i])
+        liste_cle.remove(indice_max)  # permet de retirer la valeur clé la plus elevé
+        print(liste_recette_ingre_healthy)
+        print(liste_recette_ingre_non_healthy)
+        # algo qui va remplir compte_recette
         if len(liste_recette_ingre_healthy) < 2:
             if len(liste_recette_ingre_non_healthy) < 2:
-                return liste_recette_ingre_healthy, liste_recette_ingre_non_healthy
+                compte_recette = (liste_recette_ingre_healthy, liste_recette_ingre_non_healthy)
             else:
-                return liste_recette_ingre_healthy, random.sample(liste_recette_ingre_non_healthy, 2)
+                compte_recette = (liste_recette_ingre_healthy, random.sample(liste_recette_ingre_non_healthy, 2))
         else:
             if len(liste_recette_ingre_non_healthy) < 2:
-                return random.sample(liste_recette_ingre_healthy, 2), liste_recette_ingre_non_healthy
+                compte_recette = (random.sample(liste_recette_ingre_healthy, 2), liste_recette_ingre_non_healthy)
             else:
-                return (
-                random.sample(liste_recette_ingre_healthy, 2), random.sample(liste_recette_ingre_non_healthy, 2))
+                compte_recette = (random.sample(liste_recette_ingre_healthy, 2),
+                                  random.sample(liste_recette_ingre_non_healthy, 2))
+        print('compte recette')
+        print(compte_recette)
+    return compte_recette
 
 
-print(donne_bon_compte_recette(dic2))
+print(donne_bon_compte_recette(donne_dico_index(liste_ingre, li)))
