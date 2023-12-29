548ef418ab8044a3205b782034610fbe464dbc8c
Vir <virginie2002.maurin@orange.fr>
2023-12-29 11:12:24 +0100

    version3

--- Donne_compte_recette.py
+++ Donne_compte_recette.py
@@ -1,14 +1,11 @@
 import random
 from Healthy import est_healthy
-from Creation_dic_recette_indexe2 import donne_dico_index
+from Creation_dic_recette_indexe4 import donne_dico_index
 from Part1 import donne_liste_recette
 
-# dic = {1: ['Salade', 'Pizza'], 2: ['Mayonnaise', 'Gnockie', 'Cookie'], 3: ['Repas']}
-# dic2 = {1: ['Pizza Végétarienne'], 2: ['Salade', 'Pizza'], 3: ['Bruschetta', 'Quiche aux Légumes'], 4: ['Potage de Légumes']}
+liste_ingre = [' Courgette', ' Oeuf', ' Chorizo']
+liste_intolerence=['Aubergine']
 
-
-liste_ingre = [' Champignons', 'Tofu', 'Poulet', ' Riz basmati cuit','Quinoa']
-li = donne_liste_recette()
 
 
 def donne_liste_cle(cle_dico):
@@ -34,20 +31,52 @@ def est_complet_liste_retour(lis):
     if len(lis) == 0:
         return False
     else:
-        if len(lis[0]) == 0 or len(lis[1]) == 0: #cas où un des élements de la liste est vide donc qu'il n'y a pas de proposition d'un type de recette (healthy / non helthy)
+        if len(lis[0]) == 0 or len(lis[1]) == 0:  # cas où un des élements de la liste est vide donc qu'il n'y a pas de proposition d'un type de recette (healthy / non helthy)
             return False
         else:
             return True
 
-# mettre à jour : donne_compte_recette + healthy
-def donne_bon_compte_recette(dic):
+
+def modif_compte_recette(compte_recette, liste_recette_ingre_healthy, liste_recette_ingre_non_healthy):
+    if len(compte_recette) == 0:
+        compte_recette = [[], []]
+    for liste_recette in (liste_recette_ingre_healthy,
+                          liste_recette_ingre_non_healthy):  # permet de ne faire qu'une boulce pour healthy et non healthy
+        if liste_recette == liste_recette_ingre_healthy:
+            index = 0
+        else:
+            index = 1
+        if len(liste_recette) >= 2:
+            if not compte_recette[index]:  # equivaut à faire == []
+                compte_recette[index] = random.sample(liste_recette, 2)
+            if len(compte_recette[index]) == 1:
+                compte_recette[index].append(random.sample(liste_recette, 1)[0])
+
+        elif len(liste_recette) == 1:
+            if not compte_recette[index]:  # equivaut à faire == []
+                compte_recette[index] = liste_recette[0]
+            if len(compte_recette[index]) == 1:
+                compte_recette[index].append(liste_recette[0])
+
+    return compte_recette
+
+
+
+
+def donne_bon_compte_recette(liste_ingre,liste_intolerence):
+    li = donne_liste_recette()
+    dic = donne_dico_index(liste_ingre, li,liste_intolerence)
     compte_recette = []
     liste_cle = donne_liste_cle(dic.keys())
 
-    liste_recette_ingre_healthy = []
-    liste_recette_ingre_non_healthy = []
+
     while est_complet_liste_retour(
-            compte_recette) == False and donne_cle_max(liste_cle) != -1:  # tant qu'on n'a pas au moins 1 recette healthy et non healty
+            compte_recette) == False and donne_cle_max(
+        liste_cle) != -1:  # tant qu'on n'a pas au moins 1 recette healthy et non healty
+
+        liste_recette_ingre_healthy = []
+        liste_recette_ingre_non_healthy = []
+
         # but récuperer liste des clé (pour ensuite trouver la clé max)
         # si il n'y a plus de clé cad le dictionnaire est à nouveau vide :
         if len(dic) == 0:
@@ -61,24 +90,12 @@ def donne_bon_compte_recette(dic):
                 liste_recette_ingre_healthy.append(dic[indice_max][i])
             else:
                 liste_recette_ingre_non_healthy.append(dic[indice_max][i])
+                print(dic[indice_max][i])
         liste_cle.remove(indice_max)  # permet de retirer la valeur clé la plus elevé
-        print(liste_recette_ingre_healthy)
-        print(liste_recette_ingre_non_healthy)
+
         # algo qui va remplir compte_recette
-        if len(liste_recette_ingre_healthy) < 2:
-            if len(liste_recette_ingre_non_healthy) < 2:
-                compte_recette = (liste_recette_ingre_healthy, liste_recette_ingre_non_healthy)
-            else:
-                compte_recette = (liste_recette_ingre_healthy, random.sample(liste_recette_ingre_non_healthy, 2))
-        else:
-            if len(liste_recette_ingre_non_healthy) < 2:
-                compte_recette = (random.sample(liste_recette_ingre_healthy, 2), liste_recette_ingre_non_healthy)
-            else:
-                compte_recette = (random.sample(liste_recette_ingre_healthy, 2),
-                                  random.sample(liste_recette_ingre_non_healthy, 2))
-        print('compte recette')
-        print(compte_recette)
+        compte_recette = modif_compte_recette(compte_recette,liste_recette_ingre_healthy,liste_recette_ingre_non_healthy)
+
     return compte_recette
 
-
-print(donne_bon_compte_recette(donne_dico_index(liste_ingre, li)))
+print(donne_bon_compte_recette(liste_ingre,liste_intolerence))
